5b6ad33d397f8caad15d08388d2dcca10bdcb9ad
EloisaJosue <eloisajoyjosue@gmail.com>
2024-11-04 22:14:43 +0800

    Create colorPref.js

---
+++ colorPref.js
@@ -0,0 +1,14 @@
+function colors)
+｛
+
+let favcolors - [];
+
+}
+for (let i = 0; i < 3; i++) {
+let enterColor - prompt"Enter Your Three Favorite colors.
+"I;
+facolors-push(enterColor);
+alert("Your Favorite Colors are:" + favcolors);
+let pushcolor - prompt("Add another color you like."); favcolors -push(pushcolor);
+alert("Your new colors are:" + favcolors)
+}
